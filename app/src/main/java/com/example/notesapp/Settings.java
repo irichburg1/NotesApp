4dd21d86427383a1a66642bc1244f215478adfe5
abriellek <akataria1475@gmail.com>
2023-03-06 12:09:18 -0500

    9th commit- made settings page sorting work

--- app/src/main/java/com/example/notesapp/Settings.java
+++ app/src/main/java/com/example/notesapp/Settings.java
@@ -1,9 +1,12 @@
 package com.example.notesapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +19,10 @@ public class Settings extends AppCompatActivity {
         initListButton();
         initAddButton();
         initSettingsButton();
+
+        initSettings();
+        initSortByClick();
+        initSortOrderClick();
     }
 
     private void initListButton() {
@@ -43,5 +50,81 @@ public class Settings extends AppCompatActivity {
         ImageButton ibSettings = findViewById(R.id.imageButtonSettings);
         ibSettings.setEnabled(false);
     }
+
+    private void initSettings(){
+        String sortBy = getSharedPreferences("MyNotesPreferences",
+                Context.MODE_PRIVATE).getString("sortfield","subjectname");
+        String sortOrder = getSharedPreferences("MyNotesPreferences",
+                Context.MODE_PRIVATE).getString("sortorder","ASC");
+
+        RadioButton rbName = findViewById(R.id.radioSubject);
+        RadioButton rbCity = findViewById(R.id.radioImportance);
+        RadioButton rbBirthday = findViewById(R.id.radioDate);
+        if(sortBy.equalsIgnoreCase("subjectname")) {
+            rbName.setChecked(true);
+        }
+        else if (sortBy.equalsIgnoreCase("importance")) {
+            rbCity.setChecked(true);
+        }
+        else{
+            rbBirthday.setChecked(true);
+        }
+
+        RadioButton rbAscending = findViewById(R.id.radioAscending);
+        RadioButton rbDescending = findViewById(R.id.radioDescending);
+        if(sortOrder.equalsIgnoreCase("ASC")) {
+            rbAscending.setChecked(true);
+        }
+        else{
+            rbDescending.setChecked(true);
+        }
+    }
+
+    private void initSortByClick() {
+        RadioGroup rgSortBy = findViewById(R.id.radioGroupSortBy);
+        rgSortBy.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton rbName = findViewById(R.id.radioSubject);
+                RadioButton rbCity = findViewById(R.id.radioImportance);
+                if(rbName.isChecked()) {
+                    getSharedPreferences("MyNotesPreferences",
+                            Context.MODE_PRIVATE).edit()
+                            .putString("sortfield","subjectname").apply();
+                }
+                else if (rbCity.isChecked()) {
+                    getSharedPreferences("MyNotesPreferences",
+                            Context.MODE_PRIVATE).edit()
+                            .putString("sortfield","importance").apply();
+                }
+                else{
+                    getSharedPreferences("MyNotesPreferences",
+                            Context.MODE_PRIVATE).edit()
+                            .putString("sortfield","date").apply();
+                }
+            }
+        });
+    }
+
+    private void initSortOrderClick() {
+        RadioGroup rgSortOrder = findViewById(R.id.radioGroupSortOrder);
+        rgSortOrder.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton rbAscending = findViewById(R.id.radioAscending);
+                if(rbAscending.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences",
+                            Context.MODE_PRIVATE).edit()
+                            .putString("sortorder","ASC").apply();
+                }
+                else{
+                    getSharedPreferences("MyContactListPreferences",
+                            Context.MODE_PRIVATE).edit()
+                            .putString("sortorder","DESC").apply();
+                }
+            }
+        });
+    }
+
 
 }
