18db8fba024b95c9027d1b89bb7462726ff81f3f
abriellek <akataria1475@gmail.com>
2023-03-06 12:01:44 -0500

    8th commit- made buttons work

--- app/src/main/java/com/example/notesapp/ListView.java
+++ app/src/main/java/com/example/notesapp/ListView.java
@@ -7,39 +7,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class ListView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_view);
+
 
         initListButton();
         initAddButton();
         initSettingsButton();
     }
 
+
     private void initListButton() {
         ImageButton ibList = findViewById (R.id.imageButtonList);
+        ibList.setEnabled(false);
+    }
+
+    private void initAddButton() {
+        ImageButton ibList = findViewById (R.id.imageButtonAdd);
         ibList.setOnClickListener (new View.OnClickListener () {
             public void onClick (View view) {
-                Intent intent = new Intent (MainActivity.this, ListView.class);
+                Intent intent = new Intent (ListView.this, MainActivity.class);
                 intent.setFlags (Intent.FLAG_ACTIVITY_CLEAR_TOP) ;
                 startActivity(intent);
             }
         });
     }
 
-    private void initAddButton() {
-        ImageButton ibList = findViewById (R.id.imageButtonAdd);
-        ibList.setEnabled(false);
-    }
-
     private void initSettingsButton() {
         ImageButton ibSettings = findViewById(R.id.imageButtonSettings);
         ibSettings.setOnClickListener (new View.OnClickListener () {
             public void onClick (View view) {
-                Intent intent = new Intent (MainActivity.this, Settings.class);
+                Intent intent = new Intent (ListView.this, Settings.class);
                 intent.setFlags (Intent.FLAG_ACTIVITY_CLEAR_TOP) ;
                 startActivity(intent);
             }
--- app/src/main/java/com/example/notesapp/Settings.java
+++ app/src/main/java/com/example/notesapp/Settings.java
@@ -1,5 +1,47 @@
 package com.example.notesapp;
 
-public class Settings {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Settings extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_notes);
+
+        initListButton();
+        initAddButton();
+        initSettingsButton();
+    }
+
+    private void initListButton() {
+        ImageButton ibList = findViewById (R.id.imageButtonList);
+        ibList.setOnClickListener (new View.OnClickListener () {
+            public void onClick (View view) {
+                Intent intent = new Intent (Settings.this, ListView.class);
+                intent.setFlags (Intent.FLAG_ACTIVITY_CLEAR_TOP) ;
+                startActivity(intent);
+            }
+        });
+    }
+    private void initAddButton() {
+        ImageButton ibList = findViewById (R.id.imageButtonAdd);
+        ibList.setOnClickListener (new View.OnClickListener () {
+            public void onClick (View view) {
+                Intent intent = new Intent (Settings.this, MainActivity.class);
+                intent.setFlags (Intent.FLAG_ACTIVITY_CLEAR_TOP) ;
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initSettingsButton() {
+        ImageButton ibSettings = findViewById(R.id.imageButtonSettings);
+        ibSettings.setEnabled(false);
+    }
 
 }
