4265e0b6005e501bd91a2f0f26b73e485fb133ae
isaiahrichburg <ijrichburg01@gmail.com>
2023-03-06 15:54:31 -0500

    another push- started on mainactivity

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,7 +1,38 @@
 package com.example.notesapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Calendar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,8 +48,48 @@ public class MainActivity extends AppCompatActivity {
         initListButton();
         initAddButton();
         initSettingsButton();
+
+        initToggleButton ();
+        setForEditing (false);
+
+
     }
 
+    //TOGGLEBUTTON for EDITING
+    private void initToggleButton () {
+        final ToggleButton editToggle = findViewById(R.id.toggleButtonEdit);
+        editToggle.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                setForEditing (editToggle.isChecked ());
+            }
+        });
+    }
+    private void setForEditing (boolean enabled) {
+        EditText editTitle = findViewById(R.id.editTextTitle);
+        EditText editContent = findViewById(R.id.editTextContent);
+
+        Button buttonSave = findViewById (R.id.buttonSave);
+
+        //ImageButton picture = findViewById(R.id.imageContact);
+
+        editTitle.setEnabled(enabled);
+        editContent.setEnabled(enabled);
+        //editPhone.setEnabled (enabled);
+        //editCell.setEnabled (enabled);
+        buttonSave.setEnabled (enabled);
+
+        if (enabled) {
+            editTitle.requestFocus();
+        } else {
+            ScrollView s = findViewById (R.id.scrollview);
+            s.fullScroll (ScrollView.FOCUS_UP);
+        }
+    }
+
+
+
+    //Beginning of Navigation Bar Controls
     private void initListButton() {
         ImageButton ibList = findViewById (R.id.imageButtonList);
         ibList.setOnClickListener (new View.OnClickListener () {
@@ -45,4 +116,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
